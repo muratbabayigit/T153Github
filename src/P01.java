71da0439b60eb487e304e8049322dc55303e92fc
babayigit <babayigit@gmail.com>
2024-11-30 20:15:17 +0300

    first commit

---
+++ src/P01.java
@@ -0,0 +1,23 @@
+public class P01 {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Git-Github");
+        System.out.println("Hello Github");
+
+
+        /*
+         git checkout versiyonNo
+         ile versiyonlar arası geçiş yapılabilir
+
+
+         sabitlenmek istenen versiyona gelince
+         git add .
+         git commit -m "versiyonNo"
+
+         ile o versiyon sabitlenir. Fakat sonraki veriyonlar gider
+
+
+
+         */
+    }
+}
+
---
+++ src/P02.java
@@ -0,0 +1,19 @@
+public class P02 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+/*
+    ilk oluşturulup github ile bağlanıldığında
+    github tarafından da verilen kodlar uygulanır
+
+    sonraki gönderimlerde
+
+    git add .
+    git commit -m "message"  ----> bu aşamada git üzerinde versiyonlama tamamlanmıştır. Bir sonraki komut sadece github'a göndermek içindir
+    git push
+
+    mesajları kullanılır.
+    güncellenmiş mesaj
+
+*/
